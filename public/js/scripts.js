e03796ba6f6d2b9dccf45a12dd8617e4418fa1b0
Yusef Mohamadi <yuseferi@gmail.com>
2021-01-09 17:54:04 +0100

    Add simple jquery validation

---
+++ public/js/scripts.js
@@ -0,0 +1,20 @@
+(function ($) {
+    $(document).ready(function () {
+        adjustOptions();
+        $("#longman,#linguee").change(function () {
+            adjustOptions();
+        });
+
+    });
+
+    function adjustOptions() {
+        if ($("#linguee").is(':checked')) {
+            $("#iframe").attr("disabled", false).prop('checked', true).click();
+            $("#full_html").attr("disabled", true);
+        }
+        if ($("#longman").is(':checked')) {
+            $("#full_html").attr("disabled", false).prop('checked', true).click();
+            $("#iframe").attr("disabled", true);
+        }
+    }
+})(jQuery);
